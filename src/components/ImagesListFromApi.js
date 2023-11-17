43465a23468b8fe2e60813c39fe4d1e0ce159108
Sophie Mispelter <asmispelter@yahoo.fr>
2023-11-17 13:00:47 +0100

    2.1 - WIP : Implement dynamic wikipedia URLs to get the thumbnail image of each object
    In this part, we have retrieved the thumbnail URl of each object from the Wikipedia API.
    
    However, many objects don't have an associated image.
    
    We have to find out if it's because there is none or if its bc of the custom name we've made with the addition of ' (moon)' in our dynamicWikiUrlsArray (ie. lines 23 - 26).

--- src/components/ImagesListFromApi.js
+++ src/components/ImagesListFromApi.js
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ImagesListFromApi.module.css";
 
 const ImagesListFromApi = () => {
-  const getImgHandler = async () => {
+  const [img, setImg] = useState([]);
 
+  const getImgHandler = async () => {
     const response = await fetch(
       "https://api.le-systeme-solaire.net/rest/bodies/?filter[]=bodyType,eq,moon"
     );
-    console.log("response: ", response);
+    // console.log("response: ", response);
 
     const data = await response.json();
     console.log("data: ", data);
+
+    const englishNameArray = [];
+    data.bodies.forEach((el) => {
+      //   console.log(el.englishName)
+      return englishNameArray.push(el.englishName);
+    });
+    // console.log("englishNameArray: ", englishNameArray);
+
+    const dynamicWikiUrlsArray = englishNameArray.map((el) => {
+      return `http://en.wikipedia.org/w/api.php?action=query&titles=${encodeURIComponent(
+        el
+      )}%20(moon)&prop=pageimages&format=json&pithumbsize=256&origin=*`;
+    });
+    // console.log("dynamicWikiUrlsArray: ", dynamicWikiUrlsArray);
+
+    let requests = dynamicWikiUrlsArray.map((url) => fetch(url));
+    // console.log("requests: ", requests);
+
+    Promise.all(requests)
+      .then((responses) => {
+        // console.log("response: ", responses);
+        const responseArr = responses.map((r) => r.json());
+        return Promise.all(responseArr);
+      })
+      .then((results) => {
+        // console.log("results: ", results);
+
+        let requestObjValues = [];
+        let thumbnailUrlsArray = [];
+        results.forEach((obj) => {
+          //   console.log("obj: ", obj.query.pages);
+
+          requestObjValues = Object.values(obj.query.pages);
+          //   console.log("thumbnailUrlsArray: ", thumbnailUrlsArray[0]);
+
+          if (requestObjValues[0].thumbnail === undefined) {
+            console.log("No image associated!");
+          } else {
+            // console.log(
+            //   "requestObjValues: ",
+            //   requestObjValues[0].thumbnail.source
+            // );
+            thumbnailUrlsArray.push(requestObjValues[0].thumbnail.source);
+          }
+        });
+        // console.log("thumbnailUrlsArray: ", thumbnailUrlsArray);
+        setImg(thumbnailUrlsArray);
+      });
   };
 
   return (
@@ -18,9 +67,21 @@ const ImagesListFromApi = () => {
       <button className={classes.button} role="button" onClick={getImgHandler}>
         Get Images
       </button>
-      {/* <div style={{ marginTop: "20px" }}>
-        <img src={img} />
-      </div> */}
+      <div
+        style={{
+          margin: "20px",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {img &&
+          img.map((el, i) => (
+            <div key={i} style={{ marginTop: "20px" }}>
+              <img src={el} />
+            </div>
+          ))}
+      </div>
     </>
   );
 };
